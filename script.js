36001ae6727bd92e47ea3248b12850bbab348045
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-05 10:35:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function toCase(text) {
   // write your code here
+	if(text === 0){
+		return "-";
+	}
+	const lower = text.toLowerCase();
+	const upper = text.toUpperCase();
+	return lower + "-" + upper;
 }
 
 // DO not change the code below
 
 const text = prompt("Enter text:");
 alert(toCase(text));
+
+ 
